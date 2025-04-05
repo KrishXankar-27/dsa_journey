4d67191ead6a8441a4b848ac523825a39c42f9c7
KrishXankar-27 <ihavenoenemies.27.thorfi9@gmail.com>
2025-04-05 10:22:22 +0530

    first commit of day 1

---
+++ 001_fundamental_of_c++/_002variable.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a = 10;
+  cout <<a;
+  float b = 50.22;
+  cout <<b;
+  
+  char c = 'K';
+  cout <<c;
+} 
---
+++ 001_fundamental_of_c++/_003increment__decrement.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout  <<11%2 << "\n" ; 
+    cout << 5%95;
+    return 0;
+}
